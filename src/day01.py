ad1172c1ac6699b0594437d5b55115467bef7441
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-01 18:19:51 +0100

    Solution for part2

--- src/day01.py
+++ src/day01.py
@@ -1,4 +1,5 @@
 from typing import List
+import numpy as np
 
 
 def increases(measurements: List[int]) -> int:
@@ -10,5 +11,18 @@ def increases(measurements: List[int]) -> int:
         if measurements[k] > prev:
             increments += 1
         prev = measurements[k]
+
+    return increments
+
+def window_increases(measurements: List[int]) -> int:
+    """find all the measurements where it 
+    increases from the previous windo of measurements"""
+    increments = 0
+    windows = list(map(sum, np.lib.stride_tricks.sliding_window_view(measurements, 3)))
+    prev = windows[0]
+    for k in range(1, len(windows)):
+        if windows[k] > prev:
+            increments += 1
+        prev = windows[k]
 
     return increments
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -27,6 +27,24 @@ class Day01TestSuite(unittest.TestCase):
         f = open('data/day01.txt')
         self.assertEquals(day01.increases(list(map(int, f.read().splitlines()))), 1529)
 
+    def test_measurements_window_increases(self):
+        input = [
+                199,
+                200,
+                208,
+                210,
+                200,
+                207,
+                240,
+                269,
+                260,
+                263]
+        self.assertEquals(day01.window_increases(input), 5)
+
+    def test_part2(self):
+        f = open('data/day01.txt')
+        self.assertEquals(day01.window_increases(list(map(int, f.read().splitlines()))), 1567)
+
 
 if __name__ == '__main__':
     unittest.main()
