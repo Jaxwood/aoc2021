faf1fa07f81f817ae1055a99c77353a8c10cfa91
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-03 11:30:32 +0100

    Solution for day03 part 2

--- src/day03.py
+++ src/day03.py
@@ -1,25 +1,79 @@
-from typing import List
+from typing import Counter, List, Tuple
 import numpy as np
+
+def to_int(strs: List[str]) -> List[int]:
+    """convert list of str to int"""
+    return list(map(int, strs))
+
+def count_one_zero(bits: List[int]) -> Tuple[int, int]:
+    """counts the numbers of 0 and 1 in the list"""
+    zero = 0
+    one = 0
+    for bit in bits:
+        if bit == 1:
+            one += 1
+        else:
+            zero += 1
+    return (zero, one)
+
+def transpose(nums: List[List[int]]) -> List[List[int]]:
+    return np.array(list(map(to_int, nums))).transpose()
 
 def calculate_power_consumption(instructions: List[str]) -> int:
     """calculates the power consumption
     by multiply gamma and epsilon rates"""
     gamma = 0
     epsilon = 0
-    to_int = lambda xs : list(map(int, xs))
-    diagnostics = np.array(list(map(to_int, instructions))).transpose()
+    candidates = list(map(to_int, instructions))
+    diagnostics = transpose(candidates)
     factor = len(diagnostics) - 1
     for diag in diagnostics:
-        one = 0
-        zero = 0
-        for bit in diag:
-            if bit == 1:
-                one += 1
-            else:
-                zero += 1
+        (zero, one) = count_one_zero(diag)
         if one > zero:
             epsilon += pow(2, factor)
         else:
             gamma += pow(2, factor)
         factor -= 1
     return epsilon * gamma
+
+def calculate_life_support_rating(instructions: List[str]) -> int:
+    """calculates the life support rating
+    by multiply oxygen generator and CO2 scrubber rating"""
+    diagnostics = np.array(list(map(to_int, instructions))).transpose()
+
+    oxygen = list(map(to_int, instructions))
+    for idx in range(0, len(diagnostics)):
+        (zero, one) = count_one_zero(diagnostics[idx])
+        if zero == one or one > zero:
+            oxygen = list(filter(lambda i: i[idx] == 1, oxygen))
+        else:
+            oxygen = list(filter(lambda i: i[idx] == 0, oxygen))
+        if len(oxygen) == 1:
+            break
+        diagnostics = transpose(oxygen)
+
+    diagnostics = np.array(list(map(to_int, instructions))).transpose()
+
+    co2 = list(map(to_int, instructions))
+    for idx in range(0, len(diagnostics)):
+        (zero, one) = count_one_zero(diagnostics[idx])
+        if zero == one or one > zero:
+            co2 = list(filter(lambda i: i[idx] == 0, co2))
+        else:
+            co2 = list(filter(lambda i: i[idx] == 1, co2))
+        if len(co2) == 1:
+            break
+        diagnostics = transpose(co2)
+
+
+    oxygen_total = 0
+    co2_total = 0
+    factor = len(oxygen[0]) - 1
+    for x in range(0, len(oxygen[0])):
+        if oxygen[0][x] == 1:
+            oxygen_total += pow(2, factor)
+        if co2[0][x] == 1:
+            co2_total += pow(2, factor)
+        factor -= 1
+
+    return oxygen_total * co2_total
--- tests/test_day03.py
+++ tests/test_day03.py
@@ -28,6 +28,26 @@ class Day01TestSuite(unittest.TestCase):
         f = open('data/day03.txt')
         self.assertEquals(day03.calculate_power_consumption(f.read().splitlines()), 2261546)
 
+    def test_life_support_rating(self):
+        input = [
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+            ]
+        self.assertEquals(day03.calculate_life_support_rating(input), 230)
+
+    def test_part2(self):
+        f = open('data/day03.txt')
+        self.assertEquals(day03.calculate_life_support_rating(f.read().splitlines()), 6775520)
 
 if __name__ == '__main__':
     unittest.main()
