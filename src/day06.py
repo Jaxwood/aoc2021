5c05d22b8b964828b749d96eb38d054d9af51c42
Jacob Lorenzen <jacob.lorenzen@velux.com>
2021-12-06 13:21:22 +0100

    Solution for day06 part 1

--- src/day06.py
+++ src/day06.py
@@ -1,5 +1,17 @@
 from typing import List
 
-
 def part1(raw: str, days: int) -> int:
-    return len(raw)
+    nums = map(int, raw.split(','))
+    next = []
+    while days > 0:
+        for n in nums:
+            if n == 0:
+                next.append(6)
+                next.append(8)
+            else:
+                next.append(n - 1)
+        days -= 1
+        nums = next
+        next = []
+
+    return len(nums)
