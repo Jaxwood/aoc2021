140b87dce0508e490a05d0827957558d68a3c240
Jacob Lorenzen <jacob.lorenzen@velux.com>
2021-12-07 06:44:17 +0100

    solution for day07 part 2

--- src/day07.py
+++ src/day07.py
@@ -13,3 +13,20 @@ def part1(input: str) -> int:
             best = moves
         
     return best
+
+def part2(input: str) -> int:
+    """find the best position to align on costing the least amout of moves"""
+    nums = list(map(int, input.split(',')))
+    min_pos = min(nums)
+    max_pos = max(nums)
+
+    best = None
+    for pos in range(min_pos, max_pos + 1):
+        moves = 0
+        for num in nums:
+            if pos != num:
+                moves += sum(range(0, abs(num - pos) + 1))
+        if best is None or moves < best:
+            best = moves
+
+    return best
