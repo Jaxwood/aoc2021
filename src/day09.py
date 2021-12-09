32561daffb19f473b031ec110f14841a3b0c63b3
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-09 22:44:30 +0100

    Solution for day09 part 2

--- src/day09.py
+++ src/day09.py
@@ -1,7 +1,9 @@
 from typing import Dict, List, Tuple
 from functools import reduce
 
-def low_points(cave: Dict[Tuple[int, int], int], points: List[List[int]]) -> List[Tuple[int, int]]:
+
+def low_points(cave: Dict[Tuple[int, int], int],
+               points: List[List[int]]) -> List[Tuple[int, int]]:
     """find all low points"""
     lows = []
     for x in range(0, len(points[0])):
@@ -10,21 +12,22 @@ def low_points(cave: Dict[Tuple[int, int], int], points: List[List[int]]) -> Lis
             point = cave[(x, y)]
             valid = True
 
-            if x > 0 and cave[(x-1, y)] <= point:
+            if x > 0 and cave[(x - 1, y)] <= point:
                 valid = False
-            
-            if x < len(points[0]) - 1 and cave[(x+1,y)] <= point:
+
+            if x < len(points[0]) - 1 and cave[(x + 1, y)] <= point:
                 valid = False
-                
-            if y > 0 and cave[(x, y-1)] <= point:
+
+            if y > 0 and cave[(x, y - 1)] <= point:
                 valid = False
-                
-            if y < len(points) - 1 and cave[(x,y+1)] <= point:
+
+            if y < len(points) - 1 and cave[(x, y + 1)] <= point:
                 valid = False
 
             if valid == True:
                 lows.append((x, y))
     return lows
+
 
 def part1(lines: List[str]) -> int:
     """find the sum of the lowest points"""
@@ -33,9 +36,11 @@ def part1(lines: List[str]) -> int:
     # map into dictionary
     for x in range(0, len(integers[0])):
         for y in range(0, len(integers)):
-            cave[(x,y)] = integers[y][x]
+            cave[(x, y)] = integers[y][x]
 
-    return reduce(lambda acc, x: acc + cave[x] + 1, low_points(cave, integers), 0)
+    return reduce(lambda acc, x: acc + cave[x] + 1, low_points(cave, integers),
+                  0)
+
 
 def part2(lines: List[str]) -> int:
     """find the basin of the lowest points"""
@@ -44,6 +49,31 @@ def part2(lines: List[str]) -> int:
     # map into dictionary
     for x in range(0, len(integers[0])):
         for y in range(0, len(integers)):
-            cave[(x,y)] = integers[y][x]
+            cave[(x, y)] = integers[y][x]
 
     points = low_points(cave, integers)
+    pools = []
+    for p in points:
+        visited = set()
+        q = []
+        q.append(p)
+        while len(q) > 0:
+            coord = q.pop()
+            if coord not in visited:
+                visited.add(coord)
+                (x, y) = coord
+                if x > 0 and cave[(x - 1, y)] != 9:
+                    q.append((x - 1, y))
+
+                if x < len(integers[0]) - 1 and cave[(x + 1, y)] != 9:
+                    q.append((x + 1, y))
+
+                if y > 0 and cave[(x, y - 1)] != 9:
+                    q.append((x, y - 1))
+
+                if y < len(integers) - 1 and cave[(x, y + 1)] != 9:
+                    q.append((x, y + 1))
+        pools.append(visited)
+
+    pools_sorted = map(len, sorted(pools, key=lambda x: len(x), reverse=True))
+    return reduce(lambda acc, x: acc * x, list(pools_sorted)[:3], 1)
--- tests/test_day09.py
+++ tests/test_day09.py
@@ -6,15 +6,16 @@ from src import day09
 
 class Day01TestSuite(unittest.TestCase):
     """Test Suite for Day09"""
-
-    @parameterized.expand([
-
-        ("data/day09a.txt", 15),
-        ("data/day09.txt", 508)
-    ])
+    @parameterized.expand([("data/day09a.txt", 15), ("data/day09.txt", 508)])
     def test_part1(self, filename, expected):
         f = open(filename)
         self.assertEquals(day09.part1(f.read().splitlines()), expected)
+
+    @parameterized.expand([("data/day09a.txt", 1134), ("data/day09.txt", 508)])
+    def test_part2(self, filename, expected):
+        f = open(filename)
+        self.assertEquals(day09.part2(f.read().splitlines()), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
