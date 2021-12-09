d160251ef4e187524ce8250f51abc821336837f9
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-09 19:31:22 +0100

    Refactor to use low_points method

--- src/day09.py
+++ src/day09.py
@@ -1,4 +1,30 @@
-from typing import List
+from typing import Dict, List, Tuple
+from functools import reduce
+
+def low_points(cave: Dict[Tuple[int, int], int], points: List[List[int]]) -> List[Tuple[int, int]]:
+    """find all low points"""
+    lows = []
+    for x in range(0, len(points[0])):
+        for y in range(0, len(points)):
+            # check for adjacent points
+            point = cave[(x, y)]
+            valid = True
+
+            if x > 0 and cave[(x-1, y)] <= point:
+                valid = False
+            
+            if x < len(points[0]) - 1 and cave[(x+1,y)] <= point:
+                valid = False
+                
+            if y > 0 and cave[(x, y-1)] <= point:
+                valid = False
+                
+            if y < len(points) - 1 and cave[(x,y+1)] <= point:
+                valid = False
+
+            if valid == True:
+                lows.append((x, y))
+    return lows
 
 def part1(lines: List[str]) -> int:
     """find the sum of the lowest points"""
@@ -9,26 +35,15 @@ def part1(lines: List[str]) -> int:
         for y in range(0, len(integers)):
             cave[(x,y)] = integers[y][x]
 
-    # check for low points
-    sum = 0
+    return reduce(lambda acc, x: acc + cave[x] + 1, low_points(cave, integers), 0)
+
+def part2(lines: List[str]) -> int:
+    """find the basin of the lowest points"""
+    integers = list(map(lambda xs: list(map(int, xs)), lines))
+    cave = dict()
+    # map into dictionary
     for x in range(0, len(integers[0])):
         for y in range(0, len(integers)):
-            # check for adjacent points
-            point = cave[(x, y)]
-            valid = True
+            cave[(x,y)] = integers[y][x]
 
-            if x > 0 and cave[(x-1, y)] <= point:
-                valid = False
-            
-            if x < len(integers[0]) - 1 and cave[(x+1,y)] <= point:
-                valid = False
-                
-            if y > 0 and cave[(x, y-1)] <= point:
-                valid = False
-                
-            if y < len(integers) - 1 and cave[(x,y+1)] <= point:
-                valid = False
-
-            if valid == True:
-                sum += point + 1
-    return sum
+    points = low_points(cave, integers)
