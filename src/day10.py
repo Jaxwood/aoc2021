02f08adb69a6360ff295e19e22b59a522bd2447f
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-10 18:26:15 +0100

    Solution for day10 part 2

--- src/day10.py
+++ src/day10.py
@@ -2,9 +2,11 @@ from typing import Dict, List, Tuple
 
 openings = set(['(', '<', '[', '{'])
 
+
 def is_openings(char: str) -> bool:
     """check if tag is an open tag"""
     return char in openings
+
 
 def matches_closing(char: str, last: str) -> bool:
     """check if closing tag matches previous found opening tag"""
@@ -22,6 +24,7 @@ def matches_closing(char: str, last: str) -> bool:
 
     return False
 
+
 def score(char: str) -> int:
     """score the syntax errors"""
     if char == ')':
@@ -33,6 +36,7 @@ def score(char: str) -> int:
     if char == ']':
         return 57
     return 0
+
 
 def part1(lines: List[str]) -> int:
     """find all syntax errors"""
@@ -50,3 +54,40 @@ def part1(lines: List[str]) -> int:
                     total += score(char)
 
     return total
+
+
+def part2(lines: List[str]) -> int:
+    """fix all incomplete lines and return mid score"""
+    incomplete_lines = []
+    for line in lines:
+        history = [line[0]]
+        valid = True
+        for idx in range(1, len(line)):
+            char = line[idx]
+            if is_openings(char):
+                history.append(char)
+            else:
+                last = history.pop()
+                if not matches_closing(char, last):
+                    valid = False
+        if valid:
+            incomplete_lines.append(history)
+
+    # score
+    totals = []
+    for history in incomplete_lines:
+        total = 0
+        history.reverse()
+        for char in history:
+            total *= 5
+            if char == '(':
+                total += 1
+            if char == '[':
+                total += 2
+            if char == '{':
+                total += 3
+            if char == '<':
+                total += 4
+        totals.append(total)
+
+    return sorted(totals)[len(totals) // 2]
