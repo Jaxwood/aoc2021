810ec8ac75ac92db448bbfb2210a29965f8cf0cf
Jacob Lorenzen <jacob.lorenzen@velux.com>
2021-12-12 15:48:27 +0100

    Solution for day12 part 1

--- src/day12.py
+++ src/day12.py
@@ -1,19 +1,34 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Set
 
 
 def part1(input: List[str]) -> int:
     """find the number of unqiue paths in the maze"""
     # parse the input
-    connections: List[Tuple[str, str]] = []
+    connections: Dict[str, Set[str]] = {}
     for line in input:
         f, t = line.split("-")
-        connections.append((f, t))
+        if f in connections:
+            connections[f].add(t)
+        else:
+            connections[f] = {t}
+        if t in connections:
+            connections[t].add(f)
+        else:
+            connections[t] = {f}
 
     # find all the possible paths
-    queue = list(filter(lambda kv: kv[0] == "start", connections))
-
+    queue = [('start', [])]
+    paths = []
     while len(queue) > 0:
-        f, t = queue.pop(0)
+        current, breadcrumb = queue.pop(0)
+        if current == 'end':
+            paths.append(breadcrumb + [current])
+        else:
+            for c in connections[current]:
+                if c.islower() and c not in breadcrumb:
+                    queue.append((c, breadcrumb + [current]))
+                if c.isupper():
+                    queue.append((c, breadcrumb + [current]))
 
     # return the number of unique paths
-    return len(input)
+    return len(paths)
--- tests/test_day12.py
+++ tests/test_day12.py
@@ -6,7 +6,7 @@ from src import day12
 
 class Day01TestSuite(unittest.TestCase):
     """Test Suite for Day12"""
-    @parameterized.expand([("data/day12a.txt", 10)])
+    @parameterized.expand([("data/day12a.txt", 10), ("data/day12.txt", 4411)])
     def test_part1(self, filename, expected):
         f = open(filename)
         self.assertEquals(day12.part1(f.read().splitlines()), expected)
