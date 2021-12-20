ba9c92a2e16164d66021d22d085aa4b87a1530e4
Jacob Lorenzen <jacob@lorenzen.me>
2021-12-20 09:10:58 +0100

    Solution for day6 part 2

--- src/day06.py
+++ src/day06.py
@@ -1,18 +1,44 @@
+from functools import reduce
 from typing import List
-import numpy as np
+
 
 def part1(raw: str, days: int) -> int:
-    """find the number of fish after `days` days"""
-    nums = list(map(int, raw.split(',')))
-    while days > 0:
-        until = len(nums)
-        for idx in range(0, until):
-            n = nums[idx]
-            if n == 0:
-                nums[idx] = 6
-                nums.append(8)
-            else:
-                nums[idx] = n - 1
-        days -= 1
+    starting_fish = [int(i) for i in raw.split(',')]
 
-    return len(nums)
+    # Counts how many fish have the same days left till spawn
+    current_states = {
+        0: starting_fish.count(0),
+        1: starting_fish.count(1),
+        2: starting_fish.count(2),
+        3: starting_fish.count(3),
+        4: starting_fish.count(4),
+        5: starting_fish.count(5),
+        6: starting_fish.count(6),
+        7: starting_fish.count(7),
+        8: starting_fish.count(8)
+    }
+    next_states = {}
+
+    # Repeat once per 'day'
+    for _ in range(days):
+        next_states = {
+            0: current_states[1],
+            1: current_states[2],
+            2: current_states[3],
+            3: current_states[4],
+            4: current_states[5],
+            5: current_states[6],
+            6: current_states[7],
+            7: current_states[8],
+            8: current_states[0]
+        }
+
+        # Reset adult fish at 0 to 6
+        if current_states[0] > 0:
+            next_states[6] += current_states[0]
+
+        # Move value of new_states to current_states, reset value new_states
+        current_states = next_states
+        next_states = {}
+
+    return sum(current_states.values())
