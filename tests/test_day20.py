93239c1ab764eb2a8d7db1a83f4e70bb707d84cc
Jacob Lorenzen <jacob@lorenzen.me>
2022-04-15 12:36:35 +0200

    feat: solution for day20b

--- tests/test_day20.py
+++ tests/test_day20.py
@@ -14,6 +14,14 @@ class Day20TestSuite(unittest.TestCase):
         f = open(filename)
         self.assertEquals(day20.part1(f.read().splitlines()), expected)
 
+    @parameterized.expand([
+        ("data/day20a.txt", 3351),
+        ("data/day20.txt", 15653),
+    ])
+    def test_part1(self, filename, expected):
+        f = open(filename)
+        self.assertEquals(day20.part1(f.read().splitlines(), 50), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
