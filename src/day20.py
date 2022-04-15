c5791e8420ec378c5dec083f962ef0fba138e601
Jacob Lorenzen <jacob@lorenzen.me>
2022-04-15 12:35:13 +0200

    feat: add check for infinity

--- src/day20.py
+++ src/day20.py
@@ -15,27 +15,43 @@ def neighbor_coords(x: int, y: int) -> List[Tuple[int, int]]:
             (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
 
 
-def binary_neighbors(image: List[List[str]], x: int, y: int) -> int:
+def binary_neighbors(image: List[List[str]], x: int, y: int,
+                     infinity_mark: str) -> int:
     """construct the binary number for a given pixel"""
     binary: str = ""
     for x_, y_ in neighbor_coords(x, y):
         if x_ < 0 or y_ < 0 or x_ >= len(image) or y_ >= len(image[0]):
-            binary += '0'
+            binary += '1' if infinity_mark == '#' else '0'
         else:
             binary += '1' if image[y_][x_] == '#' else '0'
 
     return int(binary, 2)
 
 
-def part1(raw: List[str]) -> int:
+def infinity_mark(algo: str, iteration: int) -> str:
+    """find the infinity mark"""
+    start = algo[0]
+    end = algo[-1]
+    if start == '#' and end == '.':
+        return '.' if iteration % 2 == 0 else '#'
+    if start == '.' and end == '#':
+        return '.'
+    if start == '#' and end == '#':
+        return '#'
+    if start == '.' and end == '.':
+        return '.'
+
+
+def part1(raw: List[str], times: int = 2) -> int:
     """find the number of lit pixels"""
     algo, image = parse(raw)
-    for i in range(2):
+    for iteration in range(times):
         output: List[str] = []
+        mark = infinity_mark(algo, iteration)
         for y in range(-1, len(image[0]) + 1):
             line: str = ""
             for x in range(-1, len(image) + 1):
-                line += algo[binary_neighbors(image, x, y)]
+                line += algo[binary_neighbors(image, x, y, mark)]
             output.append(line)
         image = output
 
--- tests/test_day20.py
+++ tests/test_day20.py
@@ -8,6 +8,7 @@ class Day20TestSuite(unittest.TestCase):
     """Test Suite for Day20"""
     @parameterized.expand([
         ("data/day20a.txt", 35),
+        ("data/day20.txt", 5647),
     ])
     def test_part1(self, filename, expected):
         f = open(filename)
