11a2f58c9a9dcbd521809696bd219ba589013527
Jacob Lorenzen <jacob.lorenzen@velux.com>
2022-11-18 21:32:51 +0100

    feat: add initial classes for day24

---
+++ tests/test_day24.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from typing import List, Tuple
+import unittest
+from parameterized import parameterized
+from src import day24
+
+
+class Day24TestSuite(unittest.TestCase):
+    """Test Suite for Day24"""
+
+    @parameterized.expand([
+        ('data/day24a.txt', 12521),
+    ])
+    def test_part1(self, filename, expected):
+        f = open(filename)
+        self.assertEquals(day24.part1(f.read().splitlines()), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
