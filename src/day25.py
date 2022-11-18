1da3c6c90ccde5f3d4d3446b9cd743e20f0eb70b
Jacob Lorenzen <jacob.lorenzen@velux.com>
2022-11-18 12:53:58 +0100

    chore: add parse method

--- src/day25.py
+++ src/day25.py
@@ -2,10 +2,15 @@ from typing import Dict, List, Tuple
 
 
 def parse(raw: List[str]) -> Dict[Tuple[int, int], str]:
-    print(raw)
-    return dict()
+    """parse the raw strings into a dictionary"""
+    seafloor: Dict[Tuple[int, int], str] = dict()
+    for line in range(len(raw)):
+        for point in range(len(raw[line])):
+            seafloor[(point, line)] = raw[line][point]
+    return seafloor
 
 
 def part1(lines: List[str]) -> int:
-    parse(lines)
+    """find the number of moves before the seacucumber stabilizes"""
+    seafloor = parse(lines)
     return 0
